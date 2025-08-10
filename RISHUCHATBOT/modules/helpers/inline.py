2653ccbc232cc4eb3cc3196e92d5507419752c0c
Rishu <vishwakarmarishabh366@gmail.com>
2025-08-10 14:31:13 +0530

    Update inline.py

--- RISHUCHATBOT/modules/helpers/inline.py
+++ RISHUCHATBOT/modules/helpers/inline.py
@@ -86,7 +86,7 @@ HELP_BTN = [
     ],
     [
         InlineKeyboardButton(text="❍ 𝐂ʟσηє ❍", callback_data="ADMINS"),
-        InlineKeyboardButton(text="❍ 𝐓ᴧɢ 𝐀ʟʟ ❍", callback_data=""),
+        InlineKeyboardButton(text="❍ 𝐀ʟʟ ❍", callback_data="MAIHUDON"),
     ],
     [
         InlineKeyboardButton(text="❍ 𝐁ᴀᴄᴋ ❍", callback_data="BACK"),
