37fe90c4e2496b0e542ebf8c6c4acfcd37045a0f
Rishu <vishwakarmarishabh366@gmail.com>
2025-08-10 16:14:13 +0530

    Update dicegame.py

--- RISHUCHATBOT/modules/dicegame.py
+++ RISHUCHATBOT/modules/dicegame.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, enums, filters
 #from config import *
 import asyncio
-from Rishuc import app as app
+from RISHUCHATBOT import RISHUCHATBOT as app
 
 from pyrogram.handlers import MessageHandler
 
