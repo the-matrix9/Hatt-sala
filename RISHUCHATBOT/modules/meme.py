e51e5f93dd98fd7ca8291096c29f9bfbb64812a2
Rishu <vishwakarmarishabh366@gmail.com>
2025-09-07 07:04:50 +0530

    Update meme.py

--- RISHUCHATBOT/modules/meme.py
+++ RISHUCHATBOT/modules/meme.py
@@ -17,7 +17,7 @@ def meme_command(client, message):
         title = data.get("title")
 
         # Mention the bot username in the caption
-        caption = f"{title}\n\nRequest by {message.from_user.mention}\nBot username: @{app.get_me().username}"
+        caption = f"{title}\n\nRequest by {message.from_user.mention}\nBot username: @DikshaChatBot"
 
         # Send the meme image to the user with the modified caption
         message.reply_photo(
